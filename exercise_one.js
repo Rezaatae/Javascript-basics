9ab0073445b303707e8c01486a3c7df9bfa27855
rezaatae <r.atae@yahoo.com>
2021-10-21 20:42:55 +0100

    First Exercise

---
+++ exercise_one.js
@@ -0,0 +1,6 @@
+alert("Welcome to your First Exercise!");
+
+var weight_pounds = prompt("What is the weight in Pounds?");
+var weight_kg = weight_pounds*0.454;
+alert("That is: "+weight_kg +" kilograms");
+console.log("Conversion Completed")
